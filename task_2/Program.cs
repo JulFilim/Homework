cbec770da93021300b12cd62e09449c546c6be7f
JulFilim <julfilim@mail.ru>
2023-04-21 19:17:12 +0300

    ДЗ_задача_№2

---
+++ task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.Write("Input number A = ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number B = ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number C = ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+ {
+Console.Write("max= ");
+Console.WriteLine(numberA);
+}
+else 
+{ if (numberB<numberC)
+{
+Console.Write("max= ");
+Console.WriteLine(numberC);
+}
+Console.Write("max= ");
+Console.WriteLine(numberB);
+}
+
+
