9960162c947933a88db6f2cfc2f0a26d9d100c55
JulFilim <julfilim@mail.ru>
2023-04-23 12:19:16 +0300

    ДЗ_задача_4

---
+++ task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число = ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 2; // cчетчик четных чисел до введенного
+
+
+while (count <= N)
+{
+    if (count < N-1)
+   {Console.Write($"{count},"); }
+   else {Console.Write(count);}
+        count = count+2;
+    }
