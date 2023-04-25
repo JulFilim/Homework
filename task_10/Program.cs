0bf2094be96741bae19b3893021f10d5f7098947
JulFilim <julfilim@mail.ru>
2023-04-25 18:05:14 +0300

    семинар 2_ задача №10

---
+++ task_10/Program.cs
@@ -0,0 +1,8 @@
+﻿ Console.Write("Введите трехзначное число = ");
+ double x = Convert.ToDouble(Console.ReadLine());
+ double x1 = x / 100;
+ double x2 = x /10;
+ double x3 = x % 10;
+ double result = Math.Floor((x1-Math.Floor(x1))*10);
+ Console.WriteLine ($"Вторая цифра введенного числа: {result}");
+
