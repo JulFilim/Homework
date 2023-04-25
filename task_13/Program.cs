364d5e339d20e3b0fa8f540776556d70fee0a962
JulFilim <julfilim@mail.ru>
2023-04-25 21:21:27 +0300

    семинар 2_ задача №13

---
+++ task_13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число x= ");
+double x = Convert.ToDouble(Console.ReadLine());
+//int count = 0; 
+if (x<100) Console.WriteLine("третьей цифры у введенного числа нет");
+else {
+     while (100<=x)
+     {
+       x=x/10;
+    }
+    double result = Math.Floor((x-Math.Floor(x))*10);    
+      Console.WriteLine($"третья цифра у введенного числа: {result}");
+}
