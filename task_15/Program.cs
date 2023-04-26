56c98c819cf64714b7b389b8c825db41f02147ee
JulFilim <julfilim@mail.ru>
2023-04-26 10:15:47 +0300

    семинар 2_задача №15

---
+++ task_15/Program.cs
@@ -0,0 +1,9 @@
+﻿ Console.Write("Введите номер дня недели = ");
+ int x = Convert.ToInt32(Console.ReadLine());
+ 
+  if (x<6) {Console.WriteLine ($"этот день - будний");}
+  else
+  {
+      Console.WriteLine ($"этот день - выходной");
+  }
+ 
