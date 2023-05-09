aa74c4865847d5fb55b7939226ed0184a2b928de
JulFilim <julfilim@mail.ru>
2023-05-09 16:51:20 +0300

    ДЗ_задача_21

---
+++ task_21/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите х координату 1й точки = ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите у координату 1й точки = ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите z координату 1й точки = ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите х координату 2й точки = ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите у координату 2й точки = ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите z координату 2й точки = ");
+double z2 = Convert.ToDouble(Console.ReadLine());
+double s= Math.Round((Math.Sqrt(((Math.Pow((x2-x1),2))+(Math.Pow((y2-y1),2))+(Math.Pow((z2-z1),2))))),2);
+Console.WriteLine($"расстояние между заданными точками=  {s}");
+
