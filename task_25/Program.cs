ac43403d56598b1e878402d532537f83dccc3c2c
JulFilim <julfilim@mail.ru>
2023-05-18 14:10:43 +0300

    seminar 4_task 25

---
+++ task_25/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число A= ");
+double A= Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число B= ");
+double B = Convert.ToDouble(Console.ReadLine());
+
+double Extent (double X, double Y)
+{
+    double result = Math.Pow(X,Y);
+        return result;
+}
+double result = Extent (A,B);
+Console.WriteLine(result);
