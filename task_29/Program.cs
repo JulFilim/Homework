7d4b06cdf84cbe650a1b17e2b59252c934b7d440
JulFilim <julfilim@mail.ru>
2023-05-21 11:42:26 +0300

    seminar 4_task 29

---
+++ task_29/Program.cs
@@ -0,0 +1,24 @@
+﻿
+void RandomArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i]= new Random().Next(0,3);
+    }
+}
+void PrintArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+RandomArray(array);
+PrintArray(array);
+
+
+
+
