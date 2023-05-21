04cd00b16335cefcac655c076e78355a15c4e718
JulFilim <julfilim@mail.ru>
2023-05-21 16:58:42 +0300

    seminar 4_task 29

--- task_29/Program.cs
+++ task_29/Program.cs
@@ -1,9 +1,9 @@
 ﻿
-void RandomArray (int[] arr)
+void RandomArray (int[] arr, int minRand,int maxRand)
 {
     for (int i = 0; i < arr.Length; i++)
     {
-        arr[i]= new Random().Next(0,3);
+        arr[i]= new Random().Next(minRand,maxRand+1);
     }
 }
 void PrintArray (int[] arr)
@@ -13,10 +13,17 @@ void PrintArray (int[] arr)
         Console.Write($"{arr[i]} ");
     }
 }
-
+System.Console.Write("Введите длину массива= \t");
 int size = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine();
+System.Console.Write("Введите начало диапазона случайных чисел= \t");
+int start = int.Parse(Console.ReadLine());
+System.Console.WriteLine();
+System.Console.Write("Введите конец диапазона случайных чисел= \t");
+int end = int.Parse(Console.ReadLine());
+System.Console.WriteLine();
 int[] array = new int[size];
-RandomArray(array);
+RandomArray(array,start,end);
 PrintArray(array);
 
 
