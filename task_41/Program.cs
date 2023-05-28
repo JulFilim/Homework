1644d070e3ba9ba4b750ae741f76365e9b2a4e64
JulFilim <julfilim@mail.ru>
2023-05-28 14:21:14 +0300

    seminar 6_task 41

---
+++ task_41/Program.cs
@@ -0,0 +1,40 @@
+﻿ string InputMass()
+{
+    Console.Write("Введенный массив вещественных чисел:  ");
+    string mass = Console.ReadLine();
+    return mass;
+    Console.WriteLine();
+}
+
+void CreateArray(string[] mass1,double[] mass2)
+{
+    for(int i=0; i<mass1.Length; i++)
+    {   mass2[i] = Double.Parse(mass1[i]);    
+        //Console.Write($"{mass2[i]} ");
+    }
+    Console.WriteLine();
+}
+void PrintArray (double [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        { Console.Write(arr[i]+" ");}
+    Console.WriteLine();
+}
+
+int Count (double []arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i]<0) count++;
+    }
+    //Console.WriteLine(count); проверка корректности выполнения логики
+    return count;
+    System.Console.WriteLine();
+}
+string mass = InputMass();
+string []splitMass =mass.Split(' ');
+double [] newMass = new double[splitMass.Length];
+CreateArray(splitMass,newMass);
+Console.WriteLine($"Количество чисел меньше 0: {Count (newMass)}");
+System.Console.WriteLine();
