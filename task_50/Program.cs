bd15052c39d01ab32eed2969b12344c0d3484f29
JulFilim <julfilim@mail.ru>
2023-05-30 16:35:41 +0300

    seminar 7_task 50

---
+++ task_50/Program.cs
@@ -0,0 +1,83 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+
+
+
+int Number()
+{
+int m =int.Parse(Console.ReadLine());
+return m;
+Console.WriteLine();
+}
+
+void NewRandomArray (double [,] arr)
+{
+    for (int i = 0; i <arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j]= new Random().Next(10,100);
+        }
+         System.Console.WriteLine();
+    }
+}
+void  SearchForMatch (double [,] arr)
+{
+    System.Console.Write("введите индекс i: ");
+    int a=Number(); 
+    int rowsize=arr.GetLength(0);
+    System.Console.Write("введите индекс j: ");
+    int b=Number();
+    int colsize=arr.GetLength(1);
+    System.Console.WriteLine();
+        
+    if ((a > rowsize) || (b>colsize) || (b > rowsize) || (a>colsize)) 
+    {System.Console.WriteLine($"такого элемента нет  ");}
+     else {
+        
+       for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+           if ((i==a)&&(j==b)) 
+           {
+            Console.WriteLine($" {arr[i,j]} ");
+           }
+        }
+        //System.Console.WriteLine();
+    }
+}
+}
+void PrintArray (double [,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+           Console.Write($" {arr[i,j]} "); 
+                   }
+        System.Console.WriteLine();
+    }
+}
+
+
+System.Console.Write("введите число строк m:   ");
+int rows= Number();
+System.Console.Write("введите число столбцов n:   ");
+int col= Number();
+double[,] array=new double[rows,col];
+NewRandomArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+SearchForMatch (array);
+System.Console.WriteLine();
